20ccaf5e6b79c18b626d46c22015785c9f92d760
deep99739 <btech10645.22@bitmesra.ac.in>
2025-12-23 07:47:53 +0530

    fix router definition

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
